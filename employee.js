06fc9f3689f0def6388443610068e555b8033f81
Ananta Padmanabha Das <anantadash@gmail.com>
2017-06-20 09:35:09 +0530

    Adding a new code for employee Search
    Adding a new code for employee Search

---
+++ employee.js
@@ -0,0 +1,302 @@
+[
+      {
+        "id": 1,
+        "name": "Lula Potter",
+        "type": "Employee",
+        "designation": "Team Lead"
+      },
+      {
+        "id": 2,
+        "name": "Espinoza Weiss",
+        "type": "Contractor",
+        "designation": "Developer"
+      },
+      {
+        "id": 3,
+        "name": "Cunningham Gaines",
+        "type": "Contractor",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 4,
+        "name": "Katharine Rowland",
+        "type": "Employee",
+        "designation": "Senior Developer"
+      },
+      {
+        "id": 5,
+        "name": "Tracey Drake",
+        "type": "Employee",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 6,
+        "name": "Lenora Santiago",
+        "type": "Employee",
+        "designation": "Team Lead"
+      },
+      {
+        "id": 7,
+        "name": "Klein Bradley",
+        "type": "Employee",
+        "designation": "Team Lead"
+      },
+      {
+        "id": 8,
+        "name": "Glenna Knox",
+        "type": "Employee",
+        "designation": "Team Lead"
+      },
+      {
+        "id": 9,
+        "name": "Nash Dyer",
+        "type": "Employee",
+        "designation": "Senior Developer"
+      },
+      {
+        "id": 10,
+        "name": "Fay Donaldson",
+        "type": "Contractor",
+        "designation": "Developer"
+      },
+      {
+        "id": 11,
+        "name": "Roberta Mcclain",
+        "type": "Employee",
+        "designation": "Manager"
+      },
+      {
+        "id": 12,
+        "name": "Noreen Conner",
+        "type": "Contractor",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 13,
+        "name": "Flores Osborne",
+        "type": "Employee",
+        "designation": "Team Lead"
+      },
+      {
+        "id": 14,
+        "name": "Adrienne Beard",
+        "type": "Contractor",
+        "designation": "Developer"
+      },
+      {
+        "id": 15,
+        "name": "Wilma Hendrix",
+        "type": "Employee",
+        "designation": "Director"
+      },
+      {
+        "id": 16,
+        "name": "Kristina Cobb",
+        "type": "Contractor",
+        "designation": "Developer"
+      },
+      {
+        "id": 17,
+        "name": "Torres Maldonado",
+        "type": "Contractor",
+        "designation": "Director"
+      },
+      {
+        "id": 18,
+        "name": "Lowery Lester",
+        "type": "Employee",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 19,
+        "name": "Shepard Arnold",
+        "type": "Employee",
+        "designation": "Developer"
+      },
+      {
+        "id": 20,
+        "name": "Etta Mcmahon",
+        "type": "Employee",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 21,
+        "name": "Mariana Holloway",
+        "type": "Employee",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 22,
+        "name": "Mills Johnson",
+        "type": "Contractor",
+        "designation": "Director"
+      },
+      {
+        "id": 23,
+        "name": "Arline Mcfarland",
+        "type": "Contractor",
+        "designation": "Developer"
+      },
+      {
+        "id": 24,
+        "name": "Chelsea Melton",
+        "type": "Contractor",
+        "designation": "Manager"
+      },
+      {
+        "id": 25,
+        "name": "Nelson Kline",
+        "type": "Contractor",
+        "designation": "Team Lead"
+      },
+      {
+        "id": 26,
+        "name": "Sutton Smith",
+        "type": "Employee",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 27,
+        "name": "Knowles Cohen",
+        "type": "Employee",
+        "designation": "Developer"
+      },
+      {
+        "id": 28,
+        "name": "Emma Park",
+        "type": "Contractor",
+        "designation": "Senior Developer"
+      },
+      {
+        "id": 29,
+        "name": "Middleton Vinson",
+        "type": "Employee",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 30,
+        "name": "Callahan Russell",
+        "type": "Employee",
+        "designation": "Senior Developer"
+      },
+      {
+        "id": 31,
+        "name": "Byers Duran",
+        "type": "Employee",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 32,
+        "name": "Head Mays",
+        "type": "Contractor",
+        "designation": "Senior Developer"
+      },
+      {
+        "id": 33,
+        "name": "Cecelia Pittman",
+        "type": "Employee",
+        "designation": "Team Lead"
+      },
+      {
+        "id": 34,
+        "name": "Harvey Cantu",
+        "type": "Contractor",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 35,
+        "name": "Carr Dillard",
+        "type": "Employee",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 36,
+        "name": "Claire Torres",
+        "type": "Employee",
+        "designation": "Director"
+      },
+      {
+        "id": 37,
+        "name": "Guerra Molina",
+        "type": "Employee",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 38,
+        "name": "Norma Silva",
+        "type": "Employee",
+        "designation": "Senior Developer"
+      },
+      {
+        "id": 39,
+        "name": "Tami Gutierrez",
+        "type": "Employee",
+        "designation": "Manager"
+      },
+      {
+        "id": 40,
+        "name": "Quinn Christian",
+        "type": "Employee",
+        "designation": "Developer"
+      },
+      {
+        "id": 41,
+        "name": "Cassandra Hardin",
+        "type": "Contractor",
+        "designation": "Manager"
+      },
+      {
+        "id": 42,
+        "name": "Lupe Blair",
+        "type": "Contractor",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 43,
+        "name": "Lilian Short",
+        "type": "Employee",
+        "designation": "Developer"
+      },
+      {
+        "id": 44,
+        "name": "Marguerite Cannon",
+        "type": "Employee",
+        "designation": "Developer"
+      },
+      {
+        "id": 45,
+        "name": "Shields Pace",
+        "type": "Employee",
+        "designation": "Assistant Manager"
+      },
+      {
+        "id": 46,
+        "name": "Fuentes Bullock",
+        "type": "Employee",
+        "designation": "Director"
+      },
+      {
+        "id": 47,
+        "name": "Penelope Montoya",
+        "type": "Contractor",
+        "designation": "Team Lead"
+      },
+      {
+        "id": 48,
+        "name": "Norton Bernard",
+        "type": "Employee",
+        "designation": "Team Lead"
+      },
+      {
+        "id": 49,
+        "name": "Margery Acosta",
+        "type": "Contractor",
+        "designation": "Team Lead"
+      },
+      {
+        "id": 50,
+        "name": "Maribel Erickson",
+        "type": "Employee",
+        "designation": "Manager"
+      }
+    ]
